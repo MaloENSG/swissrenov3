23168e48fd6d62bf69626a6e240271afa24dc034
De Lacour Malo <malo.delacour@einet.ad.eivd.ch>
2026-04-13 16:12:45 +0200

    reorganisation

--- tests/test_unitaire.py
+++ tests/test_unitaire.py
@@ -14,26 +14,26 @@ import utils as u
 
 import simple_tools as st
 
-ipath = "exemple.las"
-pc = io.read_las(ipath)
+# ipath = "exemple.las"
+# pc = io.read_las(ipath)
 
-pc.index()
+# pc.index()
 
-# Création d'un référentiel
-ref = Referentiel(x=2588822.217, 
-                  y=1242639.783, 
-                  z=451.861, 
-                  a=1.32)
+# # Création d'un référentiel
+# ref = Referentiel(x=2588822.217, 
+#                   y=1242639.783, 
+#                   z=451.861, 
+#                   a=1.32)
 
-pc_loc, bbox_glob = geo.refGlob2refLoc(pc, ref)
+# pc_loc, bbox_glob = geo.refGlob2refLoc(pc, ref)
 
-idx_class, idx_out = u.select_by_class(pc_loc, [1, 2])
-pc_class = u.select_pc_index(pc_loc, idx_class)
+# idx_class, idx_out = u.select_by_class(pc_loc, [1, 2])
+# pc_class = u.select_pc_index(pc_loc, idx_class)
 
 
-ras = st.pc_rasterise(pc_loc, mode='c', resolution=0.03, axis='x', grid_size=None)
+# ras = st.pc_rasterise(pc_loc, mode='c', resolution=0.03, axis='x', grid_size=None)
 
-plt.imshow(ras.raster)
+# plt.imshow(ras.raster)
 
 
 # opath = "exout.las"
